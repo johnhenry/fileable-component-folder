48df0b4f873ed74c90d307668e529cb8f983b96c
John Henry <john@iamjohnhenry.com>
2019-04-14 13:44:29 -0700

    update docs and tests

--- test/index.js
+++ test/index.js
@@ -1,2 +1,14 @@
+const Folder = require('..');
 const tape = require('tape');
-tape('lib test', async ({ end }) => { end(); });
+tape('Fileable Component Test: Folder', async ({deepEqual, end}) => {
+    const iterator = Folder({name:'test-folder'});
+    deepEqual(
+        (await iterator.next()).value
+        , {
+            directive: 'FOLDER'
+            , name: 'test-folder'
+            , folder_context: ''
+        },
+        'default should be created')
+    end();
+});
